a9c3e53e13f3c453e38ba76dca806e4f4188b510
Evan Langer <elanger@live.com>
2023-12-19 14:35:10 -0600

    wrote sql queries in database.sql to setup db w/ dumby data

--- database.sql
+++ database.sql
@@ -3,8 +3,32 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
+
+DROP TABLE IF EXISTS "user";
+
+DROP TABLE IF EXISTS "beers";
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "email" VARCHAR (50) NOT NULL
 );
+
+INSERT INTO "user" (
+	"username", "password", "email")
+	VALUES ('testName', '1234', 'test.com');
+	
+CREATE TABLE "beers" (
+    "id" SERIAL PRIMARY KEY,
+    "beer_name" VARCHAR (80) UNIQUE NOT NULL,
+    "notes" TEXT,
+    "is_tapped" BOOLEAN DEFAULT false,
+    "image" TEXT,
+    "user_id" INTEGER
+);
+
+INSERT INTO "beers" (
+	"beer_name", "notes",  "image")
+	VALUES ('test beer', 'a beer brewed by imagination', 'a beautiful picture');
