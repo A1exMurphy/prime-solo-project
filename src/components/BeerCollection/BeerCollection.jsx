d9e980129867fb1a6925980914fe74256bce51a3
Evan Langer <elanger@live.com>
2023-12-20 17:07:56 -0600

    added dispatch to BeerCollection.jsx

--- src/components/BeerCollection/BeerCollection.jsx
+++ src/components/BeerCollection/BeerCollection.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import NavMenu from '../NavMenu/NavMenu';
-
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 function BeerCollection() {
   const history = useHistory();
+  const dispatch  = useDispatch();
+
+  dispatch({
+    type: 'GET_COLLECTION'
+  })
 
   return (
     <div>
