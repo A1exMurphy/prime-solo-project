4d9ea01f19f0c8ffa5bd4625110f8a4253a60db3
Evan Langer <elanger@live.com>
2023-12-20 15:22:52 -0600

    added submission form to AddBrew.jsx render

--- src/components/AddBrew/AddBrew.jsx
+++ src/components/AddBrew/AddBrew.jsx
@@ -1,15 +1,70 @@
-import React from 'react';
-
+import { React, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 function AddBrew() {
   const history = useHistory();
 
+  const [beerName, setBeerName] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [notes, setNotes] = useState('');
+  //function to collect values from form inputs and dispatch to saga generator
+  const onSubmit = () => {
+
+    
+    let newBeer = {
+      beer_name: beerName,
+      image: imageUrl,
+      notes: notes
+    }
+    console.log(newBeer, "new beer")
+    
+    // dispatch({
+    //   type: "ADD_BEER",
+    //   payload: {newBeer}
+    // });
+
+    setBeerName('');
+    setImageUrl('');
+    setNotes('');
+
+  }
   return (
     <div>
 
       <center>
-        <div>Add Brew</div>
+        <form onSubmit={onSubmit}>
+          <input
+            id='beerName'
+            type='text'
+            placeholder='beer name'
+            onChange={(e) => setBeerName(e.target.value)}
+            value={beerName}
+              >
+          </input>
+
+          <input
+            id='imageUrl'
+            type='text'
+            placeholder='Image url' 
+            onChange={(e) => setImageUrl(e.target.value)}
+            value={imageUrl}
+              >        
+          </input>
+
+          <input
+            id='notes'
+            type='textbox'
+            placeholder='notes'
+            onChange={(e) => setNotes(e.target.value)}
+            value={notes}
+              >         
+          </input>
+
+          <button>
+            Add to Collection
+          </button>
+        </form>
       </center>
     </div>
   );
