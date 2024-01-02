585e102b2bd61006d3c87d2662fe18e9ba918611
Evan Langer <elanger@live.com>
2024-01-02 14:36:49 -0600

    made saga to request GET for is_tapped=true

---
+++ src/redux/sagas/tapped.saga.js
@@ -0,0 +1,27 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+
+//generator function to pass an axios GET request to server for 
+function* getTapped() {
+    // console.log('start GET request')
+
+    try {
+        const response = yield axios({
+            method: 'GET',
+            url: `/api/tapped`,
+        })
+    yield put({
+        type: 'SET_TAPPED',
+        payload: response.data
+    })
+
+} catch(error) {
+    console.log(error, 'Error in GET route')
+}};
+
+function* getTappedSaga() {
+    yield takeLatest('GET_TAPPED', getTapped);
+  }
+  
+  export default getTappedSaga;
